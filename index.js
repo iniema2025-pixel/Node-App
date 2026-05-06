8b7e28331ecab06d725b65b330543617ecf368a3
Iniobong Ema <iniema2025@gmail.com>
2026-05-06 20:52:03 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send(' DevOps Capstone App is Running!');
+});
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'OK' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
