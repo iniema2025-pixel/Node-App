b7a134d30756990bcd0109673a4241f47f13754b
Iniobong Ema <iniema2025@gmail.com>
2026-05-07 15:58:12 +0100

    test deploy

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send(' New DevOps Capstone App is Running Now!');
+  res.send(' New DevOps Capstone App is Running Now, and its great!');
 });
 
 app.get('/health', (req, res) => {
